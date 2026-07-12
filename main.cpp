53c87e7db49610ef3b49cbde29056ea8c82efeec
andersongama-dev <andersongamasilva5@gmail.com>
2026-07-12 14:19:00 -0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string listObjects[3] = {"Rock", "Paper", "Scissor"};
+    string choicePlayer = "";
+
+    cout << listObjects[0] + "\n" + listObjects[1] + "\n" + listObjects[2] + "\n";
+    cout << "Choose one of the options above.";
+
+    return 0;
+}
