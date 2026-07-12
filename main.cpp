0d5958b5d84bef177f7f5a53254fa68656d0ce89
andersongama-dev <andersongamasilva5@gmail.com>
2026-07-12 15:17:18 -0300

    feat: implementing winner verification

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main()
     cout << "Choose one of the options above: ";
     cin >> choicePlayer;
 
-    for (int i = 0; i < 2; i++)
+    for (int i = 0; i < 3; i++)
     {
         if (listObjects[i] == choicePlayer)
         {
@@ -28,13 +28,38 @@ int main()
     if (!choiceVerified)
     {
         cout << "Incorrect option!";
+        return 0;
     }
 
     random_device seed;
     mt19937 generator(seed());
     uniform_int_distribution<> distribution(0, 2);
 
-    int choiceCPU = distribution(generator);
+    int choiceCPUIndex = distribution(generator);
+    string choiceCPU = listObjects[choiceCPUIndex];
+
+    if (choicePlayer == choiceCPU)
+    {
+        cout << "\n Draw!";
+    }
+    else if (choicePlayer == "Rock" && choiceCPU == "Scissor")
+    {
+        cout << "\n You won!";
+    }
+    else if (choicePlayer == "Paper" && choiceCPU == "Rock")
+    {
+        cout << "\n You won!";
+    }
+    else if (choicePlayer == "Scissor" && choiceCPU == "Paper")
+    {
+        cout << "\n You won!";
+    }
+    else
+    {
+        cout << "\n You lost...";
+    }
+
+    cout << "\n" + choicePlayer + " x " + choiceCPU + "\n";
 
     return 0;
 }
